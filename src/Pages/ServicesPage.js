7c46d4bcf2fad91101f396a6d25e997032f96a31
Lamadrid-Mike <mikealamadrid@hotamil.com>
2022-10-10 21:34:14 -0400

    Trying Fix Service Page

--- src/Pages/ServicesPage.js
+++ src/Pages/ServicesPage.js
@@ -1,44 +1,47 @@
-import React from 'react';
-import {Card, Button} from 'react-bootstrap';
-import '../Styles/ServicesPage.css';
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import "../Styles/ServicesPage.css";
 
+function ServicesPage() {
+  const servicesInfo = [
+    {
+      Image: require("../images/rock.jpg"),
+      title: "ROCK HARD",
+      text: "Rub you down with hot rocks",
+    },
+    {
+      Image: require("../images/hotoil.jpg"),
+      title: "SEXY BACK",
+      text: "Hot oil back massage",
+    },
+    {
+      Image: require("../images/relax.jpg"),
+      title: "HAPPY ENDING",
+      text: "After your massage you get a chocolate cake",
+    },
+    {
+      Image: require("../images/rub.jpg"),
+      title: "BACK DOOR",
+      text: "Call for description",
+    },
+  ];
 
-
-function ServicesPage () {
-
-const  servicesInfo = [
-    {Image:require('../images/rock.jpg'), title:"ROCK HARD", text:"Rub you down with hot rocks"},
-    {Image: require('../images/hotoil.jpg'), title:"SEXY BACK", text:"Hot oil back massage"},
-    {Image:require('../images/relax.jpg'), title:"HAPPY ENDING", text:"After your massage you get a chocolate cake"},
-    {Image:require('../images/rub.jpg'), title:"BACK DOOR", text:"Call for description"}
-    
-];
-
-const renderCard = (card)=>{
-    return(
-  
-    <Card className="cradStyle">
+  const renderCard = (card) => {
+    return (
+      <Card id="cardStyle">
         <Card.Body>
-        <Card.Img className='imageStyle' src={card.Image}/>
-        <div className='textStyleForCard'>
-        <Card.Title className='textStyleForCardTitle'>{card.title}</Card.Title>
-        <Card.Text >{card.text}</Card.Text>
-        <Button className='buttonStyle'>TIME TO RELAX BOOK NOW</Button>
-        </div> 
+          <Card.Img id="imageStyle" src={card.Image} />
+          <div id="textStyleForCard">
+            <Card.Title id="textStyleForCardTitle">{card.title}</Card.Title>
+            <Card.Text>{card.text}</Card.Text>
+            <Button id="buttonStyle">TIME TO RELAX BOOK NOW</Button>
+          </div>
         </Card.Body>
-        
-    </Card>
-    
+      </Card>
+    );
+  };
 
-    )
-
-}
-
-  return (
-    <div>
-    {servicesInfo.map(renderCard)}
-    </div>
-  );
+  return <div>{servicesInfo.map(renderCard)}</div>;
 }
 
 export default ServicesPage;
