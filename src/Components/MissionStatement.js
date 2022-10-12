558851a215f9cc9008246003f8f5d827314ff295
Ryan Lewis <rclewis369@outlook.com>
2022-10-12 15:52:20 -0400

    Formatting and style update for MissionStatement

--- src/Components/MissionStatement.js
+++ src/Components/MissionStatement.js
@@ -13,7 +13,11 @@ const Mission = () => {
         egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend
         leo. Pellentesque habitant morbi tristique senectus et netus et malesuada
         fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
-        ultricies eget, tempor sit amet, ante. 
+        ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
+        egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend
+        leo. Pellentesque habitant morbi tristique senectus et netus et malesuada
+        fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
+        ultricies eget, tempor sit amet, ante.
       </p>
     </div>
   );
