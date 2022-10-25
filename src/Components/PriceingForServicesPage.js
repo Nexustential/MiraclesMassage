a3f0714a87bab794f56eb278ddfbcd4784d114c8
Jonathan Burke <jonathanburke747@gmail.com>
2022-10-25 18:39:14 -0400

    jonBranchServicePageUpdate

---
+++ src/Components/PriceingForServicesPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../Styles/ServicesPage.css";
+
+const Price = () => {
+  return (
+    <div className="priceingBanner">
+      <br />
+      <h1 className="titleforPriceing">PRICE</h1>
+      <p className="infoForPricing">
+        Regular: 60 min $70 | 90 min $100
+        *Specialty: 60 min $80 | 90 min $120
+      </p>
+    </div>
+  );
+};
+
+export default Price;
--- src/Pages/ServicesPage.js
+++ src/Pages/ServicesPage.js
@@ -1,29 +1,41 @@
 import React, { useState } from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button} from "react-bootstrap";
 import "../Styles/ServicesPage.css";
+import Price from "../Components/PriceingForServicesPage";
 
-function ServicesPage() {
 
+
+function ServiceCards(){
   const servicesInfo = [
     {
-      Image: require("../images/rock.jpg"),
-      title: "ROCK HARD",
-      text: "Rub you down with hot rocks",
+      Image: require("../images/massage2.jpg"),
+      title: "SWEDISH",
+      text: "Swedish massage is one of the most commonly offered massage techniques. It’s sometimes called a classic massage. The technique aims to promote relaxation by releasing muscle tension. Swedish massage may loosen up tight muscles caused by daily activities such as sitting at the computer or exercising.",
     },
     {
       Image: require("../images/hotoil.jpg"),
-      title: "SEXY BACK",
-      text: "Hot oil back massage",
+      title: "DEEP TISSUE",
+      text: "In order to reach the deep muscle tissue, a therapist will massage layer by layer of muscle, applying more and more pressure and employing special techniques with the fingers, fists, and maybe even elbows to reach and stretch that deep tissue.This type of massage facilitates healing by releasing contracted areas of muscle and tissue. It can help increase blood flow to the soft tissues and may help to reduce inflammation.",
     },
     {
       Image: require("../images/relax.jpg"),
-      title: "HAPPY ENDING",
-      text: "After your massage you get a chocolate cake",
+      title: "SPORTS*",
+      text: "Sports massage is proven to release and reduce tension in muscles, improve circulation and encourage the removal of waste products such as lactic acid during high intensity activity and can help to relax the body. All of this helps promote recovery and reduces the chance of muscle injury.",
+    },
+    {
+      Image: require("../images/massage3.jpg"),
+      title: "THAI*",
+      text: "Thai massage combines compression, acupressure, and passive stretching. These increase the range of motion in your joints and muscles. This can also improve your posture. Helps with back pain.",
     },
     {
       Image: require("../images/rub.jpg"),
-      title: "BACK DOOR",
-      text: "Call for description",
+      title: "STRUCTURAL*",
+      text: "What is a structural massage? Structural massage is a non-invasive technique that identifies the source of pain based on structural imbalances and uses body positioning to relieve tension. When you go in for a structural massage session, a professional will identify the cause of your pain by locating a tender spot",
+    },
+    {
+      Image: require("../images/rock.jpg"),
+      title: "TRIGGER POINT*",
+      text: "A trigger point is a tight area within your muscle tissue that causes pain in other parts of your body as well. They might show up as sharp, intense spasms, or as a dull generalized ache. Either way, they’re a real pain.  ",
     },
   ];
 
@@ -36,27 +48,53 @@ function ServicesPage() {
     setBookButtons(newBookButtons);
   }
 
-  const renderCard = (card, index) => {
+  
+  const RenderCard = (card, index) => {
     return (
-      <div className="servicesPage1" key={index}>
+      
+      <div className="servicesPage1 container" key={index}>
       <Card  className="servicesCardStyle">
         <Card.Body>
           <Card.Img className="servicesImageStyle" src={card.Image} />
           <div className="servicesTextStyleForCard">
             <Card.Title className="servicesTextStyleForCardTitle">{card.title}</Card.Title>
             <Card.Text>{card.text}</Card.Text>
-            <Button 
-              className="servicesButtonStyle" 
-              onClick={() => handleButtonClick(index)}
-            >{bookButtons[index] ? "Call or text us today at 555-555-5555" : "BOOK NOW!"}</Button>
+            <Button className="servicesButtonStyle" onClick={() => handleButtonClick(index)}>
+              <div className="bookButton">
+              {bookButtons[index] ? "Call or text us today at 555-555-5555" : "BOOK NOW!"}
+              </div>
+            </Button>
           </div>
         </Card.Body>
       </Card>
+      
       </div>
+      
     );
   };
 
-  return <div>{servicesInfo.map(renderCard)}</div>;
+
+
+  return(
+  <div>
+    {servicesInfo.map(RenderCard)}
+  </div>
+  )
+
+}
+
+
+
+
+
+function ServicesPage() {
+
+  return (
+  <div className="servicesPage">
+      <Price />
+      <ServiceCards />
+  </div>
+  )
 }
 
 export default ServicesPage;
